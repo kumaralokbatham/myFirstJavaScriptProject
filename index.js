676b1d90c07ffd9126e2d1739e71fd04de05f058
Alok Batham <kumaralokbatham@gmail.com>
2022-09-08 01:03:26 +0530

    Program for distinct array

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const myVar = "Hello World";
-console.log(myVar);
+const myFirstArrray = [1,2,3,4,3,8,9,3,6,9,4];
+
+// Progarm to find the distinct number form an array
+function findDistinctNumberArray(arr) {
+ const tempArr = [];
+ for (let i = 0; i<arr.length; i++) {
+    if(tempArr.indexOf(arr[i]) === -1) {
+        tempArr.push(arr[i]);
+    }
+  }
+ return tempArr;
+}
+
+const result = findDistinctNumberArray(myFirstArrray);
+console.log(result);
+
